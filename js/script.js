2e2868975854f5203a9ace53fe47239d3e4e18d3
Rafael Felix <162552577+felixwebmasterj@users.noreply.github.com>
2024-03-14 04:38:23 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,14 +6,17 @@ function search_animal(){
     let input = document.getElementById('pesquisar').value
     input=input.toLowerCase();
     let x = document.getElementsByClassName('itemSeach');
+    let y = document.getElementsByClassName('itemSeachb');
     
       
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display="none";
+            y[i].style.display="none";
         }
         else {
-            x[i].style.display="block";                 
+            x[i].style.display="block";
+            y[i].style.display="block";                 
         }
     }
 }
